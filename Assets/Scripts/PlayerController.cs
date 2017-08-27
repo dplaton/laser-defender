93ca509764476e0bcce515b87045e39524b6353a
Daniel Platon <dplaton@gmail.com>
2017-08-27 13:04:33 +0300

    Add a player spaceship and implement controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 10f;
+    public float padding = 1f;
+    float minX;
+    float maxX;
+
+    private void Start() {
+        // the distance between the camera and the object's plane
+        float distance = transform.position.z - Camera.main.transform.position.z;
+
+        // these coordinates are relative to the BOTTOM-LEFT of the screen
+        Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+
+        minX = leftMost.x + padding ;
+        maxX = rightMost.x - padding;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        float newX = Mathf.Clamp(transform.position.x, minX, maxX);
+        transform.position = new Vector3(newX, transform.position.y, 0);
+    }
+
+
+}
